d437eeab29fac571c0d308a581a7eeccf92c2202
MichelLosier <mikelosier@gmail.com>
2017-06-17 13:47:43 -0700

    added base classes for models

--- server/models/table.js
+++ server/models/table.js
@@ -1,7 +1,22 @@
-class Table {
-	constructor(){
+class Table { // session
+	let players = [];
 
+	constructor(game, players){
+		this.game = game;
+		this.players = players; //takes array
+	}
+	
+	function addPlayer (player){
+		this.players.push(player);
 	}
 }
 
+class TableManager {
+	constructor(tables) {
+		this.tables = tables;
+	}
+	function addTable (table){
+		this.tables.push(table);
+	}
+}
 export {table}
