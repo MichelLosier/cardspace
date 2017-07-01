7393ff0fb417c4d07d54edc0f8fa3a40db0af094
Bob Tribbia <btribbia@newrelic.com>
2017-07-01 13:07:49 -0700

    split up routes

---
+++ server/api/routes/root.js
@@ -0,0 +1,18 @@
+
+var rootRoutes = {
+
+}
+
+
+// GET - /api/
+rootRoutes.GET = function(req, res) {
+	res.json({"GET":"root"});
+}
+
+// POST - /api/
+rootRoutes.POST = function(req, res) {
+	res.json({"POST":"root"});
+}
+
+
+module.exports = rootRoutes;
