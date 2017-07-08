e23026dd84195fe877f96d9e84486f9457eeb8b1
Bob Tribbia <btribbia@newrelic.com>
2017-07-08 11:05:42 -0700

    cleaned up room route callbacks

--- server/api/router.js
+++ server/api/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const root = require('./routes/root');
-const room = require('./routes/room')
+const roomController = require('../controllers/roomController')
 
 const router = express.Router();
 
@@ -12,9 +12,9 @@ router.route('/').post(root.POST);
 
 // /api/room
 // /api/room/*
-router.route('/room').get(room.GET);
-router.route('/room/create').post(room.create.POST);
-router.route('/room/:id').get(room.id.GET);
-router.route('/room/:id').post(room.id.POST);
+router.get('/room', roomController.GET);
+router.post('/room/create', roomController.create.POST);
+router.get('/room/:id', roomController.id.GET);
+router.post('/room/:id', roomController.id.POST);
 
 module.exports = router;
